eabb704853cb3b665750883176aeafc0a7ab19df
Sanjay Dhanker <sanjaydhanker87@gmail.com>
2026-03-10 14:31:01 +0530

    variable

---
+++ JavaScript/01_Basic/01_Variable.js
@@ -0,0 +1,10 @@
+var name = "Sanjay";
+const accountId = 1234;
+let city = "Noida";
+anotherName = "Dev";
+
+// accountId = 9878 ;  // not allowed
+
+// prefer not to use var because of the issue in block scope and functional scope
+
+console.table([name, accountId, city, anotherName]);
