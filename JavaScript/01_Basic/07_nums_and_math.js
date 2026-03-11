8464b5f44bf8b4e8be2b4c536afb145db43e80f4
Sanjay Dhanker <sanjaydhanker87@gmail.com>
2026-03-11 12:21:40 +0530

    nums and math

---
+++ JavaScript/01_Basic/07_nums_and_math.js
@@ -0,0 +1,18 @@
+const password = 25367;
+
+const myNum = new Number(879102);
+
+// console.log(password);
+// console.log(myNum);
+
+// ===================math ===================
+
+// console.log(Math);
+// console.log(Math.abs(-3)); //negative to positive
+// console.log(Math.random()); // generate random number
+// console.log(Math.round(3.4));
+// console.log(Math.floor(7.9)); //min value
+// console.log(Math.ceil(3.1)); //max value
+
+let otp = Math.random() * 1000000;
+console.log(Math.floor(otp));
