93116a38db2d7222b0041ee6157addbf364d0b7f
Sanjay Dhanker <sanjaydhanker87@gmail.com>
2026-03-11 10:34:48 +0530

    conversion

---
+++ JavaScript/01_Basic/03_ConversionOperations.js
@@ -0,0 +1,15 @@
+let score = "44";
+
+let toNumber = Number(score);
+
+// console.log(typeof score);
+// console.log(typeof toNumber);
+
+let value = "33abc";
+
+let valueInNumber = Number(value);
+
+console.log(value); //33abc
+console.log(typeof value); //string
+console.log(valueInNumber); //NaN
+console.log(typeof valueInNumber); //Number
