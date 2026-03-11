0b027833024a516f180b7c49d14d83b8b81bcbc2
Sanjay Dhanker <sanjaydhanker87@gmail.com>
2026-03-11 10:49:06 +0530

    operation

--- JavaScript/01_Basic/03_ConversionOperations.js
+++ JavaScript/01_Basic/03_ConversionOperations.js
@@ -9,7 +9,30 @@ let value = "33abc";
 
 let valueInNumber = Number(value);
 
-console.log(value); //33abc
-console.log(typeof value); //string
-console.log(valueInNumber); //NaN
-console.log(typeof valueInNumber); //Number
+// console.log(value); //33abc
+// console.log(typeof value); //string
+// console.log(valueInNumber); //NaN
+// console.log(typeof valueInNumber); //Number
+
+//   ****************Opretion *****************************
+
+// console.log(2 + 2);
+// console.log(2 / 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 % 2);
+// console.log(2 ** 2);
+// console.log(2 ** 4);
+
+console.log(1 + "2"); //12
+console.log(1 + "2" + 1); //121
+console.log("1" + 2 + 1); //121
+console.log(1 + 2 + "1"); //31
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
