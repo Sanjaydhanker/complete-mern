f64b9a841b9d0577777e2a0f211646a9d8d1e657
Sanjay Dhanker <sanjaydhanker87@gmail.com>
2026-03-12 11:36:45 +0530

    function

--- JavaScript/03_Basic/01_Function.js
+++ JavaScript/03_Basic/01_Function.js
@@ -23,4 +23,28 @@ function loginMessage(userName) {
     console.log("Please enter a username");
   } else return `${userName} just logged in`;
 }
-console.log(loginMessage("Sanjay"));
+// console.log(loginMessage("Sanjay"));
+
+function calculateNumber(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateNumber(10, 20, 30, 50, 100));
+
+const user = {
+  userName: "Sanjay",
+  userId: "123",
+};
+
+function handleObject(anyObj) {
+  console.log(`username is ${anyObj.userName} and userId is ${anyObj.userId}`);
+}
+// handleObject(user);
+
+const myArr = [1000, 200, 300];
+
+function secondValue(value) {
+  return value[1];
+}
+
+console.log(secondValue(myArr));
