34ff638d5c885ed365121ba2e9eaa27e492df5f2
Sanjay Dhanker <sanjaydhanker87@gmail.com>
2026-05-18 11:39:22 +0530

    forEachloop

---
+++ JavaScript/05_iterations/for_each_loop.js
@@ -0,0 +1,30 @@
+const mySkills = [
+  "html",
+  "css",
+  "bootstrap",
+  "tailwind",
+  "js",
+  "reactJS",
+  "nodeJS",
+  "mongoDB",
+];
+
+// mySkills.forEach((item, index, arr) => {
+//   console.log(item, index, arr);
+// });
+
+mySkills.forEach((item) => {
+  //   console.log(item);
+});
+
+const smallApi = [
+  { userName: "Sam", age: 19 },
+  { userName: "Sanjay", age: 20 },
+  { userName: "Harsh", age: 19 },
+];
+
+smallApi.forEach((itemList) => {
+  console.log(itemList);
+  console.log(itemList.userName);
+  console.log(itemList.age);
+});
